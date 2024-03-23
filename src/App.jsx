e4ec04d9b26e9e02dfdd3a9e055a227f5a9bdb59
MrChocolate <mrchocolatepr@gmail.com>
2024-03-22 23:31:38 -0300

    Agregando react-router-dom y rutas

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,27 @@
 
 import Footer from "./Components/Footer";
 import Navbar from "./Components/Navbar";
-
+import Home from './Routes/Home'
+import Favs from './Routes/Favs'
+import Detail from './Routes/Detail'
+import Contact from '/Routes/Contact'
+import { Outlet } from "react-router-dom";
+import {Link} from 'react-router-dom'
 
 function App() {
   return (
       <div className="App">
-          <Navbar/>
-          <Footer/>
+        
+        <Navbar/>
+          <Outlet>
+            <Home/>
+            <Contact/>
+            <Detail/>
+            <Favs/>
+          </Outlet>
+        <Footer/>
+
+
       </div>
   );
 }
