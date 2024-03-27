89ccdfcf75fa08a8fd8dd8ac79ba33828712f6e1
MrChocolate <mrchocolatepr@gmail.com>
2024-03-26 23:00:57 -0300

    Agregando Validaciones y detalles finales al Form

--- src/Components/Form.jsx
+++ src/Components/Form.jsx
@@ -3,39 +3,47 @@ import React, { useState } from "react";
 
 const Form = () => {
   //Aqui deberan implementar el form completo con sus validaciones
-  const  [nombre, setNombre] = useState()
-  const  [email, setEmail]   = useState()
-  const [show, setShow] = useState(false)
 
-  const onChangeNombre = (e) => setNombre(e.target.value);
-  const onChangeEmail = (e) => setEmail(e.target.value);
+  const  [client, setClient] = useState({
+    nombre: '',
+    email: ''
+  })
+  const  [show, setShow]     = useState(false)
+  const  [err, setErr]       = useState(false)
+
+  const onChangeNombre = (e) => setClient(...client, nombre = e.target.value);
+  const onChangeEmail = (e) => setClient(...client, email= e.target.value);
   
-
   const onSubmitForm = (e) => {
 
     e.preventDefault();
-
-
+    const emailRegex = /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/
+    if(nombre.length > 5 && emailRegex.test(email)){
+      setShow(true)
+      console.log('Gracias' + nombre + ', te contactaremos cuanto antes vía mail');
+    } else {
+        setErr(true)
+      }
   }
-
   return (
     <div>
+
+      {show ? null: 
       <form onSubmit={onSubmitForm}>
         <input type="text"
         placeholder="Nombre Completo"
-        value = {nombre}
+        value = {client.nombre}
         onChange={onChangeNombre}
          />
         <input type="text"
         placeholder="Email"
-        value = {email}
+        value = {client.email}
         onChange={onChangeEmail} />
-
         <button type="submit">Enviar</button>
-
       </form>
+      }
 
-      {show ? 'esto' : null}
+      {err && <span style={{color: 'red'}}>Por favor verifique su información nuevamente</span> }
     </div>
   );
 };
